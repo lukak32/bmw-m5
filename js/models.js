e8f263e2060f4c60a1a82cb12c9aaf026eab839f
lukak32 <42774534+lukak32@users.noreply.github.com>
2020-02-25 11:09:13 +0100

    Add files via upload

---
+++ js/models.js
@@ -0,0 +1,22 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function back(n) {
+    showSlides(slideIndex += n);
+}
+
+function next(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("models__model");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        // slides[i].classList.remove("models__model--active");
+        slides[i].style.display = "none"
+    }
+    slides[slideIndex-1].style.display = "block";
+}
---
+++ js/navigation.js
@@ -0,0 +1,5 @@
+$(function() {
+    $(".navigation__icon").click(function() {
+        $(".navigation").toggleClass('navigation-open');
+    });
+});
